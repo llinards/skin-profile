ee6edf1734ce07a9bdf4ed2eeaafebf494d19acc
KarlisEmbergs <karlisembergs@gmail.com>
2025-10-27 18:13:58 +0200

    Creating recommendation logic.

---
+++ assets/recommended-products.js
@@ -0,0 +1,19 @@
+class RecommendedProducts extends HTMLElement {
+    constructor() {
+        super();
+    }
+
+    async loadRecommendations() {
+        const response = await fetch(`${this.dataset.url}&product_id=${this.dataset.productId}&section_id=${this.dataset.sectionId}`);
+        const responseHTML = await response.text();
+        const html = document.createElement('div');
+        html.innerHtml = responseHTML;
+
+        const recommendations = html.querySelector('recommended-products');
+        if (recommendations?.innerHTML.trim().length > 0) {
+            this.innerHTML = recommendations.innerHTML;
+        }
+    }
+}
+
+customElements.define("recommended-products", RecommendedProducts)
