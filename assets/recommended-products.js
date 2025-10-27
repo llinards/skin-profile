3eacf7dcbddf1761f304edaeeeb05f8e03487e67
KarlisEmbergs <karlisembergs@gmail.com>
2025-10-27 18:27:25 +0200

    Related product update.

--- assets/recommended-products.js
+++ assets/recommended-products.js
@@ -11,7 +11,7 @@ class RecommendedProducts extends HTMLElement {
         const response = await fetch(`${this.dataset.url}&product_id=${this.dataset.productId}&section_id=${this.dataset.sectionId}`);
         const responseHTML = await response.text();
         const html = document.createElement('div');
-        html.innerHtml = responseHTML;
+        html.innerHTML = responseHTML;
 
         const recommendations = html.querySelector('recommended-products');
         if (recommendations?.innerHTML.trim().length > 0) {
