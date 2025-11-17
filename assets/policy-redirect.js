827f3b5051725f935d86976f25bd4d959a5799b2
KarlisEmbergs <karlisembergs@gmail.com>
2025-11-17 22:24:15 +0200

    Redirect for policies.

---
+++ assets/policy-redirect.js
@@ -0,0 +1,15 @@
+if (window.location.pathname === '/policies/privacy-policy') {
+  window.location.replace('/pages/privacy-policy');
+}
+if (window.location.pathname === '/policies/shipping-policy') {
+  window.location.replace('/pages/shipping-policy');
+}
+if (window.location.pathname === '/policies/shipping-policy') {
+  window.location.replace('/pages/shipping-policy');
+}
+if (window.location.pathname === '/policies/refund-policy') {
+  window.location.replace('/pages/refund-policy');
+}
+if (window.location.pathname === '/policies/legal-notice') {
+  window.location.replace('/pages/legal-notice');
+}
