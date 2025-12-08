6f3be0ca8cbd58d9f226870766a683e3ae4a9fb1
KarlisEmbergs <karlisembergs@gmail.com>
2025-12-08 21:59:58 +0200

    Few improvements. Privacy-policy redirect fixes.

--- assets/policy-redirect.js
+++ assets/policy-redirect.js
@@ -1,15 +1,15 @@
-// if (window.location.pathname === '/policies/privacy-policy') {
-//   window.location.replace('/pages/privacy-policy');
-// }
-if (window.location.pathname === '/policies/shipping-policy') {
-  window.location.replace('/pages/shipping-policy');
+if (window.location.pathname === '/policies/privacy-policy') {
+  window.location.replace('/pages/privatuma-politika');
 }
 if (window.location.pathname === '/policies/shipping-policy') {
-  window.location.replace('/pages/shipping-policy');
+  window.location.replace('/pages/veikala-noteikumi');
+}
+if (window.location.pathname === '/policies/shipping-policy') {
+  window.location.replace('/pages/veikala-noteikumi');
 }
 if (window.location.pathname === '/policies/refund-policy') {
-  window.location.replace('/pages/refund-policy');
+  window.location.replace('/pages/veikala-noteikumi');
 }
 if (window.location.pathname === '/policies/legal-notice') {
-  window.location.replace('/pages/legal-notice');
+  window.location.replace('/pages/veikala-noteikumi');
 }
